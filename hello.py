ef5c177920b9f05d941a95ec516497c93ca3b1b1
Lucas <lucasleejiajin@gmail.com>
2026-09-04 10:50:16 +0800

    activity 5

--- hello.py
+++ hello.py
@@ -24,8 +24,12 @@ username = input("Enter Username: ")
 age = input("Enter Age: ")
 category = input("Enter Content Category: ")
 
+age = int(age)
 print("\n Instagram Profile")
 print("======================")
 print("Username:", username)
 print("Age: ",age)
 print("Category:", category)
+
+if age>40 and category == "fun":
+    print("You are old what is fun for you??")

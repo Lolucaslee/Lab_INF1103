2350b6dbf832ec077c5cecf9ec8c15760fe53c5f
Lucas <lucasleejiajin@gmail.com>
2026-09-10 23:50:56 +0800

    Reporting

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -1,4 +1,6 @@
 inventory_count = 0
+input_value = 0
+error_count = 0
 
 while True:
     input_value = input("Enter a stock quantity (or type 'quit' to quit): ")
@@ -11,3 +13,5 @@ while True:
     if inventory_count >= 500:
         print("The total inventory has exceeded 500 units.")
         break
+    
+print("Total units processed",inventory_count, "& Number of failed entries: ",error_count)

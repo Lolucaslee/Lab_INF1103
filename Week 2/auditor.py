0da8b5ff1c901a52f1cdf957f75bdf3548fe5bd8
Lucas <lucasleejiajin@gmail.com>
2026-09-10 23:42:30 +0800

    Handle Invalid Input

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -4,3 +4,5 @@ while True:
     input_value = input("Enter a stock quantity (or type 'quit' to quit): ")
     if input_value == 'quit':
         break
+    elif not input_value.isdigit():
+        print("Error, invalid input")    

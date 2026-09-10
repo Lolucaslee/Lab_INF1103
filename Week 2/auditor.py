f6c16172ce0860ba67fb6cbde523470231411766
Lucas <lucasleejiajin@gmail.com>
2026-09-10 23:43:07 +0800

    Enforce business rules

--- Week 2/auditor.py
+++ Week 2/auditor.py
@@ -4,5 +4,5 @@ while True:
     input_value = input("Enter a stock quantity (or type 'quit' to quit): ")
     if input_value == 'quit':
         break
-    elif not input_value.isdigit():
+    elif not input_value.isdigit() or int(input_value) < 0:
         print("Error, invalid input")    
